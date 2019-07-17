d3ddc00f8260469815a002e5091d0b4aa53c7a4e
Deepika <deepikahande06@gmail.com>
2019-07-17 20:06:03 +0530

    my first commit

---
+++ instafilters.py
@@ -0,0 +1,11 @@
+from PIL import Image
+base_img = Image.open("newpic.jpg")
+img_filter = Image.open("filter.jpg")
+size=(760,760)
+base_img=base_img.resize(size)
+img_filter=img_filter.resize(size)
+r,g,b=base_img.split()
+R,G,B=img_filter.split()
+im=Image.merge("RGB",(r,G,b))
+im.show()
+im.save('rand.jpg')
